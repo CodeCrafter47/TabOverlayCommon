c6562246d254f5e2bcb51a4ba72e9559f60095ac
Florian Stober <Flo.e3@gmx.de>
2018-07-27 16:57:41 +0200

    add loading & error icons

--- src/main/java/de/codecrafter47/taboverlay/config/icon/DefaultIconManager.java
+++ src/main/java/de/codecrafter47/taboverlay/config/icon/DefaultIconManager.java
@@ -8,6 +8,7 @@ import com.google.gson.internal.LinkedHashTreeMap;
 import de.codecrafter47.taboverlay.Icon;
 import de.codecrafter47.taboverlay.ProfileProperty;
 import de.codecrafter47.taboverlay.config.template.TemplateCreationContext;
+import de.codecrafter47.taboverlay.config.template.icon.ConstantIconTemplate;
 import de.codecrafter47.taboverlay.config.template.icon.IconTemplate;
 import de.codecrafter47.taboverlay.config.view.AbstractActiveElement;
 import de.codecrafter47.taboverlay.config.view.icon.IconView;
@@ -15,6 +16,7 @@ import de.codecrafter47.taboverlay.config.view.icon.IconViewConstant;
 import de.codecrafter47.taboverlay.config.view.icon.IconViewUpdateListener;
 import org.yaml.snakeyaml.error.Mark;
 
+import javax.annotation.Nullable;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
@@ -25,7 +27,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.ByteBuffer;
-import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -47,12 +48,15 @@ public class DefaultIconManager implements IconManager {
     private final ScheduledExecutorService tabEventQueue;
     private final Path iconFolder;
     private final Logger logger;
-    private final Cache<String, IconEntry> cache = CacheBuilder.newBuilder().weakValues().build();
+    private final Cache<String, IconTemplate> cache = CacheBuilder.newBuilder().weakValues().build();
     private final Map<IconImageData, Icon> iconCache = new ConcurrentHashMap<>();
 
     private final static Pattern PATTERN_VALID_USERNAME = Pattern.compile("(?:\\p{Alnum}|_){1,16}");
     private final static Pattern PATTERN_VALID_UUID = Pattern.compile("(?i)[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}");
     private final static Gson gson = new Gson();
+
+    private final static Icon ICON_ERROR = new Icon(new ProfileProperty("textures", "eyJ0aW1lc3RhbXAiOjE1MzI3MDEwODA1NDMsInByb2ZpbGVJZCI6ImIzYjE4MzQ1MzViZjRiNzU4ZTBjZGJmMGY4MjA2NTZlIiwicHJvZmlsZU5hbWUiOiIxMDExMTEiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2FkYzk4N2NmYmEyNDRjYzg1MGNjOGUxMmFhNGYyNzEwNzNmMTgwNWU0YjhlODliZjRmMjAxZjZkMDAzNGU0NzMifX19", "ctcSWWnrcwQusiZs3qhDComk1qmFyfls+ARVKDOW6DNGXgytqlArb1XpakDuLYGAS5WMYT0iVpuxBIO/nc7FNt/PmALDvio0PCrojokRKw4CDy14gMTf3X3fXKI0sdAoROz/K7kaodbxQxf8IUfB4BHaj81CNCvMehPfhlsPRNzk5Yb6nJwOqJQmmceMGd3Tho1OjggK56TPoLvdPePVpJZj3AovMDrrKl1WcsPjg8iSFgW3DbKvpbLlgOQ7SNvi1NTKWVrr+RmucDkHpPem2Uz7jzXJVeF40NN20lBc8Dur0q0PCx/HUdL3RuBYjgdJdG9cwhtYZlUdAOAopmSooRyNTT5Axma8pIqjkR9szKAXoTOlj1UQ5nEvSBZp10BJN2qLPdp8MDtXVLeFZZ/4uKOnyvdbjwbxQOBPjHF6Sde34hWiBAMLF977UqJMozphfD3guWuOUyb5EuGIDWxzuh8zdirNwUwrdnAs9TSmI7Lvpffc9VrzyW0xYHq2yOIMTkR5992H9KS/CfaE/oEIFinXO1rBWbKDG7PLbx5sRfclMQnLa5jEjmiDvl1mp5Id9FYOMWjgJx+LVXg1aiVJtXYtEW5jk2Y9H2123k05MT/yZGRMKli2fcy2XTxURaamZ/6sJ4g2vkg79jQEW5Si4syZu+W24xb/C+msjxUOSFQ="));
+    private final static IconTemplate ICON_TEMPLATE_ERROR = new ConstantIconTemplate(ICON_ERROR);
 
 
     public DefaultIconManager(ScheduledExecutorService asyncExecutor, ScheduledExecutorService tabEventQueue, Path iconFolder, Logger logger) {
@@ -86,7 +90,7 @@ public class DefaultIconManager implements IconManager {
             // todo contains a placeholder
             tcc.getErrorHandler().addWarning("Icon definition contains placeholder. This is not supported yet.", mark);
         } else {
-            IconEntry entry = cache.getIfPresent(s);
+            IconTemplate entry = cache.getIfPresent(s);
 
             if (entry != null) {
                 return entry;
@@ -108,21 +112,18 @@ public class DefaultIconManager implements IconManager {
             if (entry != null) {
                 cache.put(s, entry);
                 return entry;
-            } else {
-                // todo missing icon
             }
         }
-        // todo log and return error
-        return IconTemplate.STEVE;
+
+        cache.put(s, ICON_TEMPLATE_ERROR);
+        return ICON_TEMPLATE_ERROR;
     }
 
     public Map<String, Icon> getIconCache() {
         try {
             return Files.find(iconFolder, Integer.MAX_VALUE, (path, attrs) -> attrs.isRegularFile() && path.getFileName().toString().endsWith(".png"))
                     .collect(Collectors.toMap(
-                            path -> {
-                                return iconFolder.relativize(path).toString();
-                            },
+                            path -> iconFolder.relativize(path).toString(),
                             path -> {
                                 BufferedImage head;
                                 try {
@@ -338,11 +339,12 @@ public class DefaultIconManager implements IconManager {
 
     private class IconEntry implements IconTemplate {
         private Supplier<IconView> factory;
+        @Nullable
         private List<Runnable> listeners = new ArrayList<>();
 
-        public IconEntry(CompletableFuture<Icon> iconProvider) {
+        IconEntry(CompletableFuture<Icon> iconProvider) {
             factory = IconViewDelegate::new;
-            iconProvider.exceptionally(th -> Icon.DEFAULT_ALEX /* todo error icon */)
+            iconProvider.exceptionally(th -> ICON_ERROR)
                     .thenAcceptAsync(icon -> {
                         IconViewConstant iconView = new IconViewConstant(icon);
                         factory = () -> iconView;
@@ -401,7 +403,12 @@ public class DefaultIconManager implements IconManager {
 
     private static class IconViewLoading extends AbstractActiveElement<IconViewUpdateListener> implements IconView, Runnable {
         private static final long ANIMATION_INTERVAL_MS = 500;
-        private static final Icon[] ANIMATION_ELEMENTS = new Icon[]{Icon.DEFAULT_STEVE};// todo loading icons
+        private static final Icon[] ANIMATION_ELEMENTS = new Icon[]{
+                new Icon(new ProfileProperty("textures", "eyJ0aW1lc3RhbXAiOjE1MzI3MDEyMDE3OTgsInByb2ZpbGVJZCI6ImIzYjE4MzQ1MzViZjRiNzU4ZTBjZGJmMGY4MjA2NTZlIiwicHJvZmlsZU5hbWUiOiIxMDExMTEiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzZiOWEwZWI4MDEwNjExMTU2MTI2OTgxOTNkY2Y1N2VmZjJmZWU1Y2NmZThkYmZjNGYxYzZlYjcyZWRlOWVjMTIifX19", "jpj9Nn3X+B3ck+v51RFi8he3Vd5M4liKmeh/xrvsAmoK4zMOkdtz92gLnbHq0j3Sym4PCn+IHahtWbD/PF7X1bP2R3Vx2OlI19jGUUG4U9lDQYTzo5tb3mE4Vlm4eLn2ji2HWTLTgKtMLlhXDHrStDmYRFxWtt3ut5NSCVFmHw4EDvfDlvm98m0C2WcfbVY1rgZrmMBX8BJgBr95HaImrMHCXkeEA4WzuYVfeYxGK0kCxuz4HM0GwIf7ZZgDIraejpVvwpmtLn9hOogJvVzw5LvYkYrPUzcLPnZ2buskuS6HXjXf4aay/1NBX1rgjYVh3fOs/t9G5O/1Xit7MiKXekhkg0z3zq5nUsr9xnGi0MJX8ahAnB8pZMAjJ8Cir87mL7wWWk3qh2GXLiCSgBgy1DTe++WaQ2kQ0iHgiQqrwD13P2t2AM9QAQMoauyDKRcw1WTxUF2v+fLu6LMRaA3v4cXU2BuVor4/VQSXJoYrh/Tf1Can1WuvJcIrVGnvKCNtVzrDFxahr/o5GW6/bxlMQQBQnOx/Uj5litOvU3GpZaQOq6nPE5ICYkdHmQnuF3yqsfU632I9KoUge9uiuu1Gu5P9Nt1cu3OubQeN0OVrCCos7kAhRoj51IrKcTWr7FbbfnAd8RJyIkNg77zkcEq2hbrpyxrSqbrFo3fIl8k+NSY=")),
+                new Icon(new ProfileProperty("textures", "eyJ0aW1lc3RhbXAiOjE1MzI3MDEzMjMwNzksInByb2ZpbGVJZCI6ImIzYjE4MzQ1MzViZjRiNzU4ZTBjZGJmMGY4MjA2NTZlIiwicHJvZmlsZU5hbWUiOiIxMDExMTEiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2RmNjkxMDJiZjA5ZTFlNDEwMDEyZjQ4MmQ5NGVlNjk0MTEzZGU5NzhlYmI5ZDFiNDk1MmFjOGRhMTUwMDQzYmEifX19", "mm6LhFVCHUIPWxT49poJ0gX6Sv9kN5AjaU24dHg+eMOBV4xRrrPwnvlBF09uGpEjHYMlyP8jZblo3j2mlyo4N3KrwmD523XQytVLgWLl5zpgbsqckyx/cKvIbx/4Rg+NfyLktMeWISVfWgQL08lJKAjCEA3lQJmwQZtnaPgtch6tHRylK0XVHnZM4k6VVpqd2JcxJcU03cl4a9CFMu65gOFu3BHUD8M3Q8upbSDWvYx3xiUqytJbGXl8cVDbrkhhhcDlBE+SRBbHebA9tU8TzvyYekD3IGJpDgmpoafjgX8BgXi7w4KOIQDN4g5pZNAq1HHEdutz0rL4i6WAUj2r1WjBj1PAcVSIiMskAnUmABfCCNp35r0bYBQUB9er5PozLSC7M2ENby7RqWaHf/vZYfGwhJqNdxNkjIudRMoUvUlAq1Ho/43lwz6q21tiZ0ABrZ6zTYJZGtYT8sPBzMjq/zur53VrkrPOo7Rh0BqolgOEL/qoFbKOMwnaG61PUCuktmjKJ9aHd4APzcvI860WZOTz9nHMohq+QiU8ZRLqY+7rZKK4fT/Bp3uK6HXRDD3Uo1zEZ5h5cjH7kL7DO2BJpMmrLouAHEeCN/7tjn6fIXt2p2Tnlr17oR2bRr1QgaEY9ef3i+z3GzWjfXWAkhfTw8idPS/ODhjA0Pip3uDRPtg=")),
+                new Icon(new ProfileProperty("textures", "eyJ0aW1lc3RhbXAiOjE1MzI3MDExNDExNjQsInByb2ZpbGVJZCI6ImIzYjE4MzQ1MzViZjRiNzU4ZTBjZGJmMGY4MjA2NTZlIiwicHJvZmlsZU5hbWUiOiIxMDExMTEiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzNlZjliZjU0NmU5YzhiYTFkNDAxZjU1NzA4NTRkNzRhYjViYzU3MWNjYjhiMzI4Mjc2MWE0ODZjMzdmYmU2ZjMifX19", "UsQFdQrtzXx3TW8uiTx2E0n7EieUPw7R55XWb5Sf+jGeAYlag9SohPGoEUt3kMFjJxanf4XN77B587vTW1GvV37tVFhF2TyBnXj+7ZKv1ly9QRV+nsq+w3MmtWfASgLAt+v4KH9R/glt/eeVa6tH9a0RSNrdWFPDggQUaM9dz2kjCuhdBFocB4jfoX9p7rJSqgRijiayLWET4PQ+KxQZ87rz0McJrZkOOoFoCvcQjZINO/YJam1YpRTgMR3l67MICn6azRRRIqlId87D0KAOltDrN15A2z1eUGktzUXxn198AhZz4l2oYXeq5a2wSx7BWPFqM/g7Q5fdV8yqAteFcFC0YUZfrO4S3uPZF2vlA1zAI3C3J5A1/SjuzNbIRLaS6h/e5sXZQNotFhY7nPPE8fN3neMFLgk+BMs3oEDozXI7y4wfep1Ss39wYF3EgIBlcaKZmc0CFyNrgD7b9q4DOYme6KagPlCas2ULU98tWmWad1mdBeoSgRXrXGdJfzlfRCnlk6aPkiOXbKfiFvxbWWAh6Nyc74z1Sm+/X68JzHs1i9Ni28rM/2hduDvTLl+bZW6Ooanuzr4IG9fh0/Y/1qd9UY0apYW+vRFTkNUMvyfvM30TO5OhDFrNHJJ5aZwOUE4Urz9dAQyzNR6DxZNR/vAAvxrzetz3poQOedM3HcU=")),
+                new Icon(new ProfileProperty("textures", "eyJ0aW1lc3RhbXAiOjE1MzI3MDEyNjI0MzQsInByb2ZpbGVJZCI6ImIzYjE4MzQ1MzViZjRiNzU4ZTBjZGJmMGY4MjA2NTZlIiwicHJvZmlsZU5hbWUiOiIxMDExMTEiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzFlYThiMWRlNTIzOGJlOGNkZjljOTYwZWMzYzNlYThmMjU4MDE4NDgwMzlhYmE1ZTJmZGUzMWE1NjdjMGFlMTcifX19", "kUaH7sUPqaQ5UUjHG0v49JU1uRcptX3Lk+BGeapM9bOrGTQzWr1ZZVG+gKH+FaXN5Q6R0ZTE3hCSW15LLjPe1499Ku4UnFtEm1F5/pQREjPiaPLNosqKBdw4K2PnkzflrkU0hHShTEruSTiDS85sa8+70wcX3y30QDvPhENwd11kVr1/sUavhzTHGQUe/AgkpTQxtOe84UeDXRG8O8QES4VUZxgNMv0A9AoXie6HaxPw0aspQkO/shUJUHja92tThm/QpqH97FhNrSfeD7xeJEcts0Nb71MAtCTXrNNycb0Ujdu2nrYCHZhAWj8BBI31DMYzOzcmjtp45CyN6TgbQUCgQcZysRbepD8Bv8NTFAVTphw40wYMqF6N7i0+kSqlYHV9M3NraNIXTOBQl+qWnRSmU0oUw5xxK+vbsJvZB8aAHe3eamdwaVIIyfupdXy1WYH1Yi5VFhF8ISdYo40+tTErBdYQKEgvd1N99VhuMerEAdyu2foKilN6B5Z4a0fNxyLwTxTDCjLR+u9kF7JLhMrB78RMTlWpnXYbcx9mgn+OBkdAHKYXZRj75UjuvlsRTjPf29KpJwgowW2/ZrI/OSE7sFU5TJVGtVgyetSKbs4AqaYWaWUbM9ZutithmR00Oan2oY0R6VdDUEMqF1NzCEZ6tJdIeKNnxrUt3MwnhFE="))
+        };
         private ScheduledFuture<?> animationUpdateTask;
         private int currentElement = 0;
 

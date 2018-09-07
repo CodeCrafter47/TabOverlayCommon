84557a473cd396b6bb9143344b805f7eb1a32fdd
Florian Stober <Flo.e3@gmx.de>
2018-09-07 10:29:47 +0200

    fix

--- src/main/java/de/codecrafter47/taboverlay/config/area/RectangularAreaWithOffset.java
+++ src/main/java/de/codecrafter47/taboverlay/config/area/RectangularAreaWithOffset.java
@@ -101,8 +101,8 @@ public class RectangularAreaWithOffset implements RectangularArea {
     public RectangularArea createRectangularChild(int column, int row, int columns, int rows) {
         Preconditions.checkArgument(column < sizeColumns, "column < sizeColumns");
         Preconditions.checkArgument(row < sizeRows, "row < sizeRows");
-        Preconditions.checkArgument(column + columns < sizeColumns, "column + columns < sizeColumns");
-        Preconditions.checkArgument(row + rows < sizeRows, "row + rows < sizeRows");
+        Preconditions.checkArgument(column + columns <= sizeColumns, "column + columns <= sizeColumns");
+        Preconditions.checkArgument(row + rows <= sizeRows, "row + rows <= sizeRows");
         return new RectangularAreaWithOffset(delegate, this.offsetColumns + column, this.offsetRows + row, columns, rows);
     }
 

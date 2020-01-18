068080a352b4b6f574fa8dd00e7a7e37f719ce4f
Florian Stober <flo.e3@gmx.de>
2020-01-18 14:42:52 +0100

    optimize TextViewAnimated

--- src/main/java/de/codecrafter47/taboverlay/config/view/text/TextViewAnimated.java
+++ src/main/java/de/codecrafter47/taboverlay/config/view/text/TextViewAnimated.java
@@ -7,17 +7,18 @@ import de.codecrafter47.taboverlay.config.view.AbstractActiveElement;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class TextViewAnimated extends AbstractActiveElement<TextViewUpdateListener> implements TextView {
 
     private Future<?> task;
-    private final List<TextTemplate> elements; // todo using text views instead of templates here might improve performance
+    private final List<TextView> elements;
     private TextView activeElement;
     private int nextElementIndex;
     private final long intervalMS;
 
     public TextViewAnimated(float interval, List<TextTemplate> elements) {
-        this.elements = elements;
+        this.elements = elements.stream().map(TextTemplate::instantiate).collect(Collectors.toList());
         this.intervalMS = (long) (interval * 1000);
     }
 
@@ -31,7 +32,7 @@ public class TextViewAnimated extends AbstractActiveElement<TextViewUpdateListen
         if (nextElementIndex >= elements.size()) {
             nextElementIndex = 0;
         }
-        activeElement = elements.get(nextElementIndex++).instantiate();
+        activeElement = elements.get(nextElementIndex++);
         activeElement.activate(getContext(), hasListener() ? getListener() : null);
         if (hasListener()) {
             getListener().onTextUpdated();
@@ -41,7 +42,7 @@ public class TextViewAnimated extends AbstractActiveElement<TextViewUpdateListen
     @Override
     protected void onActivation() {
         task = getContext().getTabEventQueue().scheduleAtFixedRate(this::switchActiveElement, intervalMS, intervalMS, TimeUnit.MILLISECONDS);
-        activeElement = elements.get(0).instantiate();
+        activeElement = elements.get(0);
         activeElement.activate(getContext(), hasListener() ? getListener() : null);
         nextElementIndex = 1;
     }

bad2e0fff1c7dea92a6ada2fc6b4c00a854fc9fc
Florian Stober <flo.e3@gmx.de>
2020-01-18 14:44:36 +0100

    clean up TabOverlayView

--- src/main/java/de/codecrafter47/taboverlay/config/view/TabOverlayView.java
+++ src/main/java/de/codecrafter47/taboverlay/config/view/TabOverlayView.java
@@ -4,7 +4,6 @@ import de.codecrafter47.taboverlay.TabView;
 import de.codecrafter47.taboverlay.config.context.Context;
 import de.codecrafter47.taboverlay.config.template.AbstractTabOverlayTemplate;
 import de.codecrafter47.taboverlay.handler.HeaderAndFooterOperationMode;
-import de.codecrafter47.taboverlay.handler.TabOverlayHandle;
 import de.codecrafter47.taboverlay.handler.TabOverlayHandler;
 
 public class TabOverlayView {
@@ -13,14 +12,14 @@ public class TabOverlayView {
     private final AbstractActiveElement<?> headerFooterView;
 
     public static TabOverlayView create(TabView tabView, TabOverlayHandler handler, Context context, AbstractTabOverlayTemplate template) {
-        AbstractActiveElement headerFooterView;
+        AbstractActiveElement<?> headerFooterView;
         if (template.showHeaderAndFooter()) {
             headerFooterView = new HeaderAndFooterView(template, handler.enterHeaderAndFooterOperationMode(HeaderAndFooterOperationMode.CUSTOM));
         } else {
             handler.enterHeaderAndFooterOperationMode(HeaderAndFooterOperationMode.PASS_TROUGH);
             headerFooterView = null;
         }
-        AbstractActiveElement contentView = template.createContentView(handler);
+        AbstractActiveElement<?> contentView = template.createContentView(handler);
 
         return new TabOverlayView(context, contentView, headerFooterView);
     }
@@ -28,8 +27,6 @@ public class TabOverlayView {
     private TabOverlayView(Context context, AbstractActiveElement<?> contentView, AbstractActiveElement<?> headerFooterView) {
         this.contentView = contentView;
         this.headerFooterView = headerFooterView;
-        // todo need this?
-        //this.logger = new ChildLogger(tabView.getLogger(), tabOverlayTemplate.getPath().toString());
 
         this.contentView.activate(context, null);
         if (headerFooterView != null) {
